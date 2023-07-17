ccd8ae340b25022ba2e70929847ce01c4220e1fa
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-07-18 00:29:33 +0100

    js file added, little changes on css anf html file

---
+++ youtube-thumbnail/index.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+
+document.addEventListener('click', () => {
+
+    btn.style.color = "black";
+    btn.style.transition = "all .4s ease-in-out";
+    btn.style.backgroundColor = "#efe4e4";
+    btn.style.border = "1px solid #0C041C "
+
+})
